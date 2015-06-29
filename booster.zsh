a521471cf1f02870321362b7415e274f9aeb352c
afifsohaili <mafif@booster.com>
2015-06-29 15:44:22 +0800

    Booster deploy - basic

--- booster.zsh
+++ booster.zsh
@@ -121,7 +121,19 @@ function hound_check() {
 function booster_deploy() {
   [ -s "$HOME/.rvm/scripts/rvm" ] && . "$HOME/.rvm/scripts/rvm"
 
+  branch=`current_branch`
+  app=$(pwd | rev | cut -d"/" -f 1 | rev)
+  capistrano_app=$($app | cut -d"_" -f 1)
+
   cd /Users/afifsohaili/Booster/autotomy
+
+  eval "cap apps:booster_$capistrano_app"":staging deploy:clear_remote_cache"
+  if [ "$branch" = "master" ]; then
+    eval "cap apps:booster_$capistrano_app"":staging deploy"
+  else
+    eval "BRANCH=$branch cap apps:booster_$capistrano_app"":staging deploy"
+  fi
+  cd "/Users/afifsohaili/Booster/$capistrano_app"
 }
 
 function booster_start {
