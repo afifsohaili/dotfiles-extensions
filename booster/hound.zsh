044f34058c903dc4d7c8586737195676b3d883df
afifsohaili <mafif@booster.com>
2015-07-30 11:38:16 +0800

    Check only Ruby files with Rubocop, not erb files

--- booster/hound.zsh
+++ booster/hound.zsh
@@ -2,7 +2,7 @@ function hound_check() {
   files=("$@")
   for file in "${files[@]}"
   do
-    if [[ "$file" == *rb ]]; then
+    if [[ "$file" == *.rb ]]; then
       rubocop -c .ruby-style.yml $file
     elif [[ "$file" == *css ]]; then
       scss-lint -c .scss-style.yml $file
