5ac8ce2353a36561dd5b17d24cb687d2d26607f2
afifsohaili <mafif@booster.com>
2015-08-24 10:24:42 +0800

    Updated Booster Tmux script

--- booster/bin/booster-tmux.sh
+++ booster/bin/booster-tmux.sh
@@ -4,9 +4,8 @@ tmux new -d -s Booster -n fulcrum
 tmux send-keys -t Booster:1 "cd ~/Booster/fulcrum" C-m
 tmux new-window -n styleguide
 tmux send-keys -t Booster:2 "cd ~/Booster/styleguide" C-m
-tmux new-window -n rails-server
-tmux send-keys -t Booster:3 "cd ~/Booster/dev_proxy" C-m
-tmux send-keys -t Booster:3 "booster_start"
+tmux new-window -n cas_server
+tmux send-keys -t Booster:3 "cd ~/Booster/cas_server" C-m
 tmux new-window -n yadr
 tmux send-keys -t Booster:4 "cd ~/.yadr/zsh" C-m
 tmux new-window -n deployments
