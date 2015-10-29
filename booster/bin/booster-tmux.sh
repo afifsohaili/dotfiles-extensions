77d3014c329d929df8b09deb4b02863f3066c170
afifsohaili <afifnajib@gmail.com>
2015-10-29 19:20:07 +0800

    Booster tmux updates

--- booster/bin/booster-tmux.sh
+++ booster/bin/booster-tmux.sh
@@ -2,11 +2,9 @@
 
 tmux new -d -s Booster -n fulcrum
 tmux send-keys -t Booster:1 "cd ~/Booster/fulcrum" C-m
-tmux new-window -n styleguide
-tmux send-keys -t Booster:2 "cd ~/Booster/styleguide" C-m
-tmux new-window -n cas_server
-tmux send-keys -t Booster:3 "cd ~/Booster/cas_server" C-m
 tmux new-window -n yadr
-tmux send-keys -t Booster:4 "cd ~/.yadr/zsh" C-m
+tmux send-keys -t Booster:2 "cd ~/.yadr/zsh" C-m
+tmux new-window -n integration_test
+tmux send-keys -t Booster:3 "cd ~/Booster/integration_test" C-m
 tmux new-window -n deployments
-tmux send-keys -t Booster:5 "cd ~/Booster/autotomy" C-m
+tmux send-keys -t Booster:4 "cd ~/Booster/autotomy" C-m
