c6c1310264901e02e79560cb0a804fd2f08ec99e
afifsohaili <afifnajib@gmail.com>
2016-01-28 11:38:37 +0800

    Update projekihsan tmux panes

--- projekihsan/projekihsan-tmux.sh
+++ projekihsan/projekihsan-tmux.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 tmux new -d -s ProjekIhsan -n development
-tmux split-window -h
+tmux split-window -v
 tmux select-pane -t 2
 tmux send-keys "cd ~/Projects/meteor/projekihsan && meteor run -p 7000 --settings server/environments/local.json" C-m
-tmux split-window -v
+tmux split-window -h
 tmux select-pane -t 3
+tmux send-keys "cd ~/Projects/meteor/projekihsan" C-m
+tmux split-window -h
+tmux select-pane -t 4
 tmux send-keys "cd ~/Projects/meteor/projekihsan" C-m
 tmux new-window -n deployment
 tmux send-keys -t ProjekIhsan:2 "cd ~/Projects/meteor/projekihsan/.production" C-m
