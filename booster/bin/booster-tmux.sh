d043b171d3c3f87cfbbe625d72bf9fcf1d81e11a
afifsohaili <afifnajib@gmail.com>
2016-02-29 11:20:48 +0800

    Limit Booster tmux windows to just 2

--- booster/bin/booster-tmux.sh
+++ booster/bin/booster-tmux.sh
@@ -9,10 +9,6 @@ tmux split-window -h
 tmux send-keys -t Booster:1 "cd ~/Booster/fulcrum" C-m
 tmux split-window -h
 tmux send-keys -t Booster:1 "cd ~/Booster/fulcrum" C-m
-tmux new-window -n yadr
-tmux send-keys -t Booster:2 "cd ~/.yadr/zsh" C-m
-tmux new-window -n integration_test
-tmux send-keys -t Booster:3 "cd ~/Booster/integration_test" C-m
 tmux new-window -n deployments
-tmux send-keys -t Booster:4 "cd ~/Booster/autotomy" C-m
+tmux send-keys -t Booster:2 "cd ~/Booster/autotomy" C-m
 tmux at -t Booster
