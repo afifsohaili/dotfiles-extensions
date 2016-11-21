796399c29041ff72e73467b4004e77a2764bb074
afifsohaili <mafif@booster.com>
2016-11-21 11:21:33 +0800

    allow debugging vpn up

--- booster/vpn.zsh
+++ booster/vpn.zsh
@@ -1,7 +1,11 @@
 function vup () {
   if [[ -n $1 ]]
   then
-    printf "mafif\n$VPN_CREDENTIAL\ny" | /opt/cisco/anyconnect/bin/vpn -s connect ci$1.out.customink.com
+    if [ "$DEBUG" = "1" ]; then
+      printf "mafif\n$VPN_CREDENTIAL\ny" | /opt/cisco/anyconnect/bin/vpn -s connect ci$1.out.customink.com
+    else
+      printf "mafif\n$VPN_CREDENTIAL\ny" | /opt/cisco/anyconnect/bin/vpn -s connect ci$1.out.customink.com | grep state
+    fi
   else
     print "Missing server location"
   fi
