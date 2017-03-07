bdbb7ef33ea8848f13b4a4e56958694ed3b11c74
afifsohaili <mafif@booster.com>
2017-03-07 15:41:22 +0800

    Update to projekihsan tmux

--- projekihsan/projekihsan-tmux.sh
+++ projekihsan/projekihsan-tmux.sh
@@ -3,18 +3,16 @@
 tmux new -d -s ProjekIhsan -n development
 tmux split-window -v
 tmux select-pane -t 2
-tmux send-keys "cd ~/Projects/meteor/projekihsan && meteor run -p 7000 --settings server/environments/local.json" C-m
+tmux send-keys "cd ~/Projects/meteor/tuisyen-sayang-app && meteor run -p 7000 --settings server/environments/local.json" C-m
 tmux split-window -h
 tmux select-pane -t 3
-tmux send-keys "cd ~/Projects/meteor/projekihsan" C-m
+tmux send-keys "cd ~/Projects/meteor/tuisyen-sayang-app" C-m
 tmux split-window -h
 tmux select-pane -t 4
-tmux send-keys "cd ~/Projects/meteor/projekihsan" C-m
-tmux new-window -n deployment
-tmux send-keys -t ProjekIhsan:2 "cd ~/Projects/meteor/projekihsan/.production" C-m
+tmux send-keys "cd ~/Projects/meteor/tuisyen-sayang-app/.production" C-m
 tmux select-window -t ProjekIhsan:1
 tmux select-pane -t 1
-tmux send-keys "cd ~/Projects/meteor/projekihsan" C-m
+tmux send-keys "cd ~/Projects/meteor/tuisyen-sayang-app" C-m
 tmux send-keys "vim" C-m
 tmux select-layout main-horizontal
 tmux at -t ProjekIhsan
