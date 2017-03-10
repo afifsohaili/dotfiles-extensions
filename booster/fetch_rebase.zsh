5b691c6ab5ff8964be40656f58ca1f6f30673b86
afifsohaili <mafif@booster.com>
2017-03-10 11:34:44 +0800

    add branch_reset

--- booster/fetch_rebase.zsh
+++ booster/fetch_rebase.zsh
@@ -7,3 +7,11 @@ function fetch_rebase() {
 }
 
 alias gfrb='fetch_rebase'
+
+function branch_reset() {
+  if [ -n "$1" ]; then
+    git fetch && git reset --hard origin/$1
+  else
+    printf "No branch given"
+  fi
+}
