52cb8dbfda7d3a6f65a59cc213132f8b7eba14ec
Afif Sohaili <afifnajib@gmail.com>
2018-09-13 19:13:28 +0800

    Add vis-cloud and link-cloud to confluence-cloud tmux script.

--- service-rocket/confluence-cloud.sh
+++ service-rocket/confluence-cloud.sh
@@ -14,11 +14,50 @@ tmux resize-pane -L 100
 
 tmux new-window
 tmux select-window -t 2
-tmux send-keys -t conf-cloud:2.1 "cd ~/ServiceRocket/jira-automations" C-m
+tmux send-keys -t conf-cloud:2.1 "cd ~/ServiceRocket/redir-cloud/" C-m
 tmux split-window -h
-tmux send-keys -t conf-cloud:2.2 "cd ~/ServiceRocket/jira-automations" C-m
+tmux send-keys -t conf-cloud:2.2 "cd ~/ServiceRocket/redir-cloud/" C-m
 tmux split-window -h
-tmux send-keys -t conf-cloud:2.3 "cd ~/ServiceRocket/jira-automations" C-m
+tmux send-keys -t conf-cloud:2.3 "cd ~/ServiceRocket/redir-cloud/" C-m
+tmux split-window -h
+tmux send-keys -t conf-cloud:2.4 "cd ~/ServiceRocket/redir-cloud/" C-m
+tmux select-layout main-vertical
+tmux resize-pane -R 100000
+tmux resize-pane -L 100
+
+tmux new-window
+tmux select-window -t 3
+tmux send-keys -t conf-cloud:3.1 "cd ~/ServiceRocket/vis-cloud/" C-m
+tmux split-window -h
+tmux send-keys -t conf-cloud:3.2 "cd ~/ServiceRocket/vis-cloud/" C-m
+tmux split-window -h
+tmux send-keys -t conf-cloud:3.3 "cd ~/ServiceRocket/vis-cloud/" C-m
+tmux split-window -h
+tmux send-keys -t conf-cloud:3.4 "cd ~/ServiceRocket/vis-cloud/" C-m
+tmux select-layout main-vertical
+tmux resize-pane -R 100000
+tmux resize-pane -L 100
+
+tmux new-window
+tmux select-window -t 4
+tmux send-keys -t conf-cloud:4.1 "cd ~/ServiceRocket/link-cloud/" C-m
+tmux split-window -h
+tmux send-keys -t conf-cloud:4.2 "cd ~/ServiceRocket/link-cloud/" C-m
+tmux split-window -h
+tmux send-keys -t conf-cloud:4.3 "cd ~/ServiceRocket/link-cloud/" C-m
+tmux split-window -h
+tmux send-keys -t conf-cloud:4.4 "cd ~/ServiceRocket/link-cloud/" C-m
+tmux select-layout main-vertical
+tmux resize-pane -R 100000
+tmux resize-pane -L 100
+
+tmux new-window
+tmux select-window -t 5
+tmux send-keys -t conf-cloud:5.1 "cd ~/ServiceRocket/jira-automations" C-m
+tmux split-window -h
+tmux send-keys -t conf-cloud:5.2 "cd ~/ServiceRocket/jira-automations" C-m
+tmux split-window -h
+tmux send-keys -t conf-cloud:5.3 "cd ~/ServiceRocket/jira-automations" C-m
 tmux select-layout main-vertical
 tmux resize-pane -R 100000
 tmux resize-pane -L 100
